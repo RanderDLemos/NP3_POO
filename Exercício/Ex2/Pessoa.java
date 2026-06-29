19c6198eefbeb30174dee81d957085bd4f8207e8
RanderDLemos <162617589+RanderDLemos@users.noreply.github.com>
2026-06-29 19:35:08 -0300

    Create Pessoa class with getters and setters

---
+++ Exercício/Ex2/Pessoa.java
@@ -0,0 +1,24 @@
+package Exercicios.Ex2;
+
+public class Pessoa {
+    private String nome;
+    private int idade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        if(idade >= 0) {
+            this.idade = idade;
+        }
+    }
+}

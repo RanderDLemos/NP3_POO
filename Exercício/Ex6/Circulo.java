4f4d45a3a4dad0a4851bb172104d04ea21fdeeba
RanderDLemos <162617589+RanderDLemos@users.noreply.github.com>
2026-06-29 23:26:16 -0300

    Add Circulo class for circle area calculation

---
+++ Exercício/Ex6/Circulo.java
@@ -0,0 +1,15 @@
+package br.inatel.formas;
+
+public class Circulo extends Forma{
+    private double raio;
+
+    public Circulo(String cor, double raio) {
+        super(cor);
+        this.raio = raio;
+    }
+
+    @Override
+    public double area() {
+        return Math.PI*raio*raio;
+    }
+}
